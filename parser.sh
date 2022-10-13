b0c1b569db7758176a08655427c1dd2ffed2f0dc
Charles Webb <v-chawebb@microsoft.com>
2022-10-13 15:13:22 -0500

    Added entry for SR and Time Zone entry as well as basic template

--- parser.sh
+++ parser.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+read -p "SR#: " srnum
+read -p "Time Zone of User: " tzuser
+
 file=$1
 sed -i 's/\t/,/g' $file
 varsub=($(awk -F ',' '/Resource Uri/ {print $2}' $file | awk -F '/' '{print $3}'))
 varrg=($(awk -F ',' '/Resource Uri/ {print $2}' $file | awk -F '/' '{print $5}'))
 varvm=($(awk -F ',' '/Resource Uri/ {print $2}' $file | awk -F '/' '{print $9}'))
 
+echo "TIME ZONE: $tzuser"
+echo "SR# $srnum"
+echo -e "\n \nISSUE: \n \n"
+echo -e "PROBLEM STATEMENT: \n \n"
+
+echo -e "ENVIRONMENT \n"
+echo -e "=============================================================\n"
 echo "Subscription: $varsub"
 echo "Resource URI: $(\awk -F ',' '/Resource Uri/ {print $2}' $file)"
 echo "Resource Group: $varrg"
@@ -17,4 +27,6 @@ echo "Region: $(\awk -F ',' '/^Region/ {print $2}' $file)"
 echo "OS: $(\awk -F ',' '/^GuestOSVersion/ {print $2}' $file)"
 echo "OS Created From: $(\awk -F ',' '/^OS Created From/ {print $2}' $file)"
 echo "Size: $(\awk -F ',' '/^Size/ {print $2}' $file)"
-
+echo -e "\n \n"
+echo -e "CX VERBATIM\n"
+echo -e "============================================================="
